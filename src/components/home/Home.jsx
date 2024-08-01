3307df198fc80221b6dbac2749bb0e2ee1e2b1df
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-08-01 12:16:43 +1000

    complete home component implementation

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,26 @@
 import "./home.css";
+import Me from "../../assets/avatar-1.svg"
+import HeaderSocials from "./HeaderSocials.jsx";
+import ScrollDown from "./ScrollDown.jsx";
+import Shapes from "./Shapes.jsx";
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <section className="home container" id="home">
+            <div className="intro">
+                <img src={Me} alt="" className="home__img"/>
+                <h1 className="home__name">Bolby Doe</h1>
+                <span className="home__education">I'm a Front-End developer</span>
+
+                <HeaderSocials />
+
+                <a href="#contact" className="btn">Hire Me</a>
+
+                <ScrollDown />
+            </div>
+
+            <Shapes />
+        </section>
     )
 }
 
