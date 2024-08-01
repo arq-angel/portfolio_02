eeea1f7c528c8a1a8aee1eaf8adc8fcea6f9c08e
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-08-01 13:38:20 +1000

    incomplete implementation of about component

---
+++ src/components/about/AboutBox.jsx
@@ -0,0 +1,43 @@
+const AboutBox = () => {
+    return (
+        <div className="about_boxes grid">
+            <div className="about__box">
+                <i className="about__icon icon-fire"></i>
+
+                <div>
+                    <h3 className="about__title">198</h3>
+                    <span className="about__subtitle">Project completed</span>
+                </div>
+            </div>
+
+            <div className="about__box">
+                <i className="about__icon icon-cup"></i>
+
+                <div>
+                    <h3 className="about__title">5670</h3>
+                    <span className="about__subtitle">Cups of coffee</span>
+                </div>
+            </div>
+
+            <div className="about__box">
+                <i className="about__icon icon-people"></i>
+
+                <div>
+                    <h3 className="about__title">427</h3>
+                    <span className="about__subtitle">Satisfied clients</span>
+                </div>
+            </div>
+
+            <div className="about__box">
+                <i className="about__icon icon-badge"></i>
+
+                <div>
+                    <h3 className="about__title">35</h3>
+                    <span className="about__subtitle">Nominees winner</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutBox;
